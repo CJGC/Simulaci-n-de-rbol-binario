59358a804ea600da59bd96b15a866904eabccd07
Christhian Gomez <chjugomez@utp.edu.co>
2018-10-21 07:46:35 -0500

    Nodo para AVL

--- src/AVL/Nodo.java
+++ src/AVL/Nodo.java
@@ -1,40 +1,74 @@
 package AVL;
+
 /**
  *
  * @author cj
  */
-public class Nodo {
+public class Nodo extends binarytree.Nodo {
     
-        private Integer nivel_izq;
-        private Integer nivel_der;
-        private String estado;
-        private Integer valor;
-	private Nodo hijo_der;
-	private Nodo hijo_izq;
-	private Nodo padre;
-        
-        public Nodo() {
-            nivel_izq = null;
-            nivel_der = null;
-            estado = null;
-        }
-        
-        public Integer getNivel_izq() {
-            return nivel_izq;
-        }
-        public void setNivel_izq(Integer nivel_izq) {
-            this.nivel_izq = nivel_izq;
-        }
-        public Integer getNivel_der() {
-            return nivel_der;
-        }
-        public void setNivel_der(Integer nivel_der) {
-            this.nivel_der = nivel_der;
-        }
-        public String getEstado() {
-            return estado;
-        }
-        public void setEstado(String estado) {
-            this.estado = estado;
-        }
+    private Integer nivel_izq;
+    private Integer nivel_der;
+    private String estado;
+    private Nodo hijo_izq;
+    private Nodo hijo_der;
+    private Nodo padre;
+    
+    public Nodo() {
+        nivel_izq = 0;
+        nivel_der = 0;
+        estado = "";
+        hijo_izq = null;
+        hijo_der = null;
+        padre = null;
+    }
+    
+    public Nodo(Integer valor) {
+        super.setValor(valor);
+        nivel_izq = 0;
+        nivel_der = 0;
+        estado = "";
+        hijo_izq = null;
+        hijo_der = null;
+        padre = null;
+    }
+    
+    public Integer getNivel_izq() {
+        return nivel_izq;
+    }
+    public void setNivel_izq(Integer nivel_izq) {
+        this.nivel_izq = nivel_izq;
+    }
+    public Integer getNivel_der() {
+        return nivel_der;
+    }
+    public void setNivel_der(Integer nivel_der) {
+        this.nivel_der = nivel_der;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    @Override
+    public Nodo getHijo_izq() {
+        return hijo_izq;
+    }
+    public void setHijo_izq(Nodo hijo_izq) {
+        this.hijo_izq = hijo_izq;
+    }
+    @Override
+    public Nodo getHijo_der() {
+        return hijo_der;
+    }
+    public void setHijo_der(Nodo hijo_der) {
+        this.hijo_der = hijo_der;
+    }
+    @Override
+    public Nodo getPadre() {
+        return padre;
+    }
+    public void setPadre(Nodo padre) {
+        this.padre = padre;
+    }
 }
