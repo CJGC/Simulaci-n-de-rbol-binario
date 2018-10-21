3bf462ab09d72ef54b7d130e94e9ab3c10929b82
Christhian Gomez <chjugomez@utp.edu.co>
2018-10-21 07:10:02 -0500

    Were introduced new attributes

--- src/binarytree/Nodo.java
+++ src/binarytree/Nodo.java
@@ -6,6 +6,9 @@ package binarytree;
  */
 public class Nodo {
 
+        private Integer left_level;
+        private Integer right_level;
+        private String status;
 	private Integer valor;
 	private Nodo hijo_der;
 	private Nodo hijo_izq;
@@ -24,7 +27,25 @@ public class Nodo {
             hijo_der = null;
             padre = null;
         }
-        
+
+        public Integer getLeft_level() {
+            return left_level;
+        }
+        public void setLeft_level(Integer left_level) {
+            this.left_level = left_level;
+        }
+        public Integer getRight_level() {
+            return right_level;
+        }
+        public void setRight_level(Integer right_level) {
+            this.right_level = right_level;
+        }
+        public String getStatus() {
+            return status;
+        }
+        public void setStatus(String status) {
+            this.status = status;
+        }
 	public Integer getValor() {
 		return valor;
 	}
