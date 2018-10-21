250eaa660bf9baeeae05e6335732f9272444dbd8
Christhian Gomez <chjugomez@utp.edu.co>
2018-10-21 07:14:03 -0500

    Se eliminarion atributos

--- src/binarytree/Nodo.java
+++ src/binarytree/Nodo.java
@@ -6,9 +6,6 @@ package binarytree;
  */
 public class Nodo {
 
-        private Integer left_level;
-        private Integer right_level;
-        private String status;
 	private Integer valor;
 	private Nodo hijo_der;
 	private Nodo hijo_izq;
@@ -28,24 +25,6 @@ public class Nodo {
             padre = null;
         }
 
-        public Integer getLeft_level() {
-            return left_level;
-        }
-        public void setLeft_level(Integer left_level) {
-            this.left_level = left_level;
-        }
-        public Integer getRight_level() {
-            return right_level;
-        }
-        public void setRight_level(Integer right_level) {
-            this.right_level = right_level;
-        }
-        public String getStatus() {
-            return status;
-        }
-        public void setStatus(String status) {
-            this.status = status;
-        }
 	public Integer getValor() {
 		return valor;
 	}
